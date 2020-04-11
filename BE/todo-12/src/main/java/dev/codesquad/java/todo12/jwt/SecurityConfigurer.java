bfae3f65d221f8e12a6347de7dacff6d983f7006
Jenny <huji3156@gmail.com>
2020-04-11 21:20:25 +0900

    [feat] JwtUtil 클래스추가 - 토큰을 만드는 메소드 생성 : generateToken, createToken - 토큰의 expired 됐는지 아닌지 확인하는 boolean 생성 : isTokenExpired

--- BE/todo-12/src/main/java/dev/codesquad/java/todo12/jwt/SecurityConfigurer.java
+++ BE/todo-12/src/main/java/dev/codesquad/java/todo12/jwt/SecurityConfigurer.java
@@ -1,9 +1,14 @@
 package dev.codesquad.java.todo12.jwt;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
         @Autowired
@@ -12,5 +17,10 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(AuthenticationManagerBuilder auth) throws Exception {
             auth.userDetailsService(myUserDetailsService);
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return NoOpPasswordEncoder.getInstance();
         }
     }
